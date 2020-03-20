065328973b8a9e2a897105ab435f7e064f3a465a
gennadiy123 <shelukhin_g@ukr.net>
2020-03-20 15:51:53 +0200

    Updates

---
+++ precache-manifest.6ce8d94da575d4bf526d6d01a87c7215.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e999051dd2f3ccbe883cf7344d28280",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "101b72ea1bc0078fa38c",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.f9b03cd2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.f9b03cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c99a917b6adebeb6551",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.d2c6f14e.chunk.js"
+  },
+  {
+    "revision": "32ca8000c2e5093c0bc4",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.fb1c9189.js"
+  }
+]);
